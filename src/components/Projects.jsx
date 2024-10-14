6de0d2311f0ed3de3b72e3ff7350e814f3594490
cyroscorp <arjunexisted@gmail.com>
2024-10-15 02:01:24 +0530

    d

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -174,7 +174,6 @@ export default function Card() {
 
 
 
-
           <div className="flex flex-col">
             {projects.map((project, index) => (
               <a
@@ -182,7 +181,7 @@ export default function Card() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 className="drop-shadow-md card bg-white rounded-lg px-5 py-3 gap-x-3 flex flex-col md:flex-none md:flex-row hover:-translate-y-1 hover:scale-100 duration-300 transition ease-in-out delay-150 hover:shadow-sm border border-gray-200 hover:border-gray-300"
-                href="https://youtu.be/L2o21EHE0lo?si=MlcGs9hLDVNBc0hf"
+                href="https://proectnight.vercel.app/"
               >
                 <div className="rounded-full overflow-hidden flex items-center justify-center border border-gray-200  md:block">
                   <div className="card-image w-16 h-16 rounded-full overflow-hidden">
@@ -195,10 +194,10 @@ export default function Card() {
                 </div>
                 <div className="flex flex-col justify-center">
                   <h1 className="font-medium text-lg">
-                    LethalGun
+                    Project
                   </h1>
                   <p className="text-gray-500 text-md">
-                    From Scratch
+                    Building from Design
                   </p>
                 </div>
                 <button className="ml-auto hidden md:inline-block">
@@ -220,7 +219,6 @@ export default function Card() {
               </a>
             ))}
           </div>
-
 
 
 
